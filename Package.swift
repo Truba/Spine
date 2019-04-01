d534c4c7972d1b6478e0baff53799ee5f152ae9f
Vlaho <vlaho.poluta@infinum.hr>
2019-04-01 09:22:09 +0200

    Update

--- Package.swift
+++ Package.swift
@@ -22,7 +22,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Spine",
-            dependencies: ["BrightFutures", "SwiftyJSON", "Result"]),
+            dependencies: ["BrightFutures", "SwiftyJSON", "Result"],
+            path: "Sources"
+            ),
         .testTarget(
             name: "SpineTests",
             dependencies: ["Spine"]),
